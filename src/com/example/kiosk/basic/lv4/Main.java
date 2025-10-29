dbb11c7b96ae67eb150d6b714c4d7988af0a3614
jangse0 <syjang1016@korea.ac.kr>
2025-10-30 06:06:41 +0900

    feat: 필수lv4 기능 완성

---
+++ src/com/example/kiosk/basic/lv4/Main.java
@@ -0,0 +1,36 @@
+package com.example.kiosk.basic.lv4;
+
+import com.example.kiosk.basic.lv4.kiosk.Kiosk;
+import com.example.kiosk.basic.lv4.menu.Menu;
+import com.example.kiosk.basic.lv4.menu.MenuItem;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args){
+        List<MenuItem> burgerItems = Arrays.asList(
+                new MenuItem("ShackBurger", 6.9, "토마토, 양상추, 쉑소스가 토핑된 치즈버거"),
+                new MenuItem("SmokeShack", 8.9, "베이컨, 체리 페퍼에 쉑소스가 토핑된 치즈버거"),
+                new MenuItem("Cheeseburger", 6.9, "포테이토 번과 비프패티, 치즈가 토핑된 치즈버거"),
+                new MenuItem("Hamburger", 5.4, "비프패티를 기반으로 야채가 들어간 기본버거")
+        );
+
+        Menu burgerMenu = new Menu("Burgers", burgerItems);
+
+        List<MenuItem> drinkItems = Arrays.asList(); // 음료 카테고리에 메뉴가 없는 경우
+        Menu drinkMenu = new Menu("Drinks", drinkItems);
+
+        List<MenuItem> dessertItems = Arrays.asList(); // 디저트 카테고리에 메뉴가 없는 경우
+        Menu dessertMenu = new Menu("Desserts", dessertItems);
+
+        List<Menu> menus= new ArrayList<>();
+
+        menus.add(burgerMenu);
+        menus.add(drinkMenu);
+        menus.add(dessertMenu);
+
+        Kiosk kiosk = new Kiosk(menus);
+        kiosk.start();
+
+    }
+}
---
+++ src/com/example/kiosk/basic/lv4/menu/Menu.java
@@ -0,0 +1,26 @@
+package com.example.kiosk.basic.lv4.menu;
+
+import java.util.List;
+
+public class Menu {
+    private String category;
+    private List<MenuItem> items;
+
+    public Menu(String category, List<MenuItem> items) {
+        this.category = category;
+        this.items = items;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public List<MenuItem> getItems() {
+        return items;
+    }
+
+
+}
